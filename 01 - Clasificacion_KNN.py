62caf1da919395c4905d366d8df2f75f61412168
Felipe Pérez <felipepepe@gmail.com>
2019-08-08 12:34:59 +0200

    Añadir parametros a KNeighborsClassifier

--- 01 - Clasificacion_KNN.py
+++ 01 - Clasificacion_KNN.py
@@ -8,7 +8,14 @@ print(iris.keys())
 
 x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target)
 
-knn = KNeighborsClassifier(n_neighbors=2)
+knn = KNeighborsClassifier(algorithm = 'auto',
+                     leaf_size = 30,
+                     metric = 'minkowski',
+                     metric_params = None,
+                     n_jobs = 1,
+                     n_neighbors = 7,
+                     p = 2,
+                     weights = 'uniform')
 
 knn.fit(x_train, y_train)
 
